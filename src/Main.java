260e68996f9b1667d6f790c07d6c2effb6ec5cc1
ismailcimencom <ismlcmn@gmail.com>
2021-11-17 22:18:56 +0300

    Asal Sayilar

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sayac = 1;
+
+        for (int i = 2; i <= 100; i++) {
+            for (int j = 2; j <= i; j++) {
+                if (i % j == 0) {
+                    sayac++;
+                }
+            }
+            if (sayac == 2) {
+                System.out.print(i + " ");
+                sayac = 1;
+            } else {
+                sayac = 1;
+            }
+        }
+    }
+}
